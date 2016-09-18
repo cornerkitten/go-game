41a547bdb09c61c25474fd83f176af7b211164f0
Sam Vaughn <cornerkitten@users.noreply.github.com>
2016-09-18 11:00:49 -0400

    Added stone placement preview with mouseover event of board.  Updated board so that wood is drawn as static image, while grid is drawn dynamically.

--- js/SpriteRenderer.js
+++ js/SpriteRenderer.js
@@ -26,3 +26,16 @@ SpriteRenderer.prototype.render = function(spriteLocation, pos) {
 	let image = this.sprites.get(spriteLocation).image;
 	this.ctx.drawImage(image, pos.x, pos.y, image.width, image.height);
 };
+
+SpriteRenderer.prototype.drawGrid = function(size, cellSize, offset, color) {
+	this.ctx.lineWidth = 0.5;
+	this.ctx.strokeStyle = color;
+	this.ctx.beginPath();
+	for (let i = 0; i < size; i++) {
+		this.ctx.moveTo(offset + i * cellSize,			offset);
+		this.ctx.lineTo(offset + i * cellSize,			offset + (size - 1) * cellSize);
+		this.ctx.moveTo(offset, 						offset + i * cellSize);
+		this.ctx.lineTo(offset + (size - 1) * cellSize,	offset + i * cellSize);
+	}
+	this.ctx.stroke();
+};
--- js/app.js
+++ js/app.js
@@ -1,9 +1,9 @@
 /*global SpriteRenderer*/
 /*eslint no-console: "off"*/
 
-var setup = function() {
+function setup() {
 	const CANVAS_ID = 'game';
-	const GAME_BOARD_IMG_SRC = 'img/game-board.png';
+	const GAME_BOARD_IMG_SRC = 'img/go-board-wood-576px.png';
 	const STONE_IMG_SRC = 'img/go-stone-black.png';
 
 	var gameCanvas = document.getElementById(CANVAS_ID);
@@ -16,16 +16,43 @@ var setup = function() {
 		spriteRenderer.render(GAME_BOARD_IMG_SRC, {x:0, y:0});
 
 		spriteRenderer.preloadSprite(STONE_IMG_SRC, () => {
-			spriteRenderer.render(STONE_IMG_SRC, {x:16, y:16});
+			
+			spriteRenderer.drawGrid(9, 64, 32, 'black');
 		});
 	});
 
 	gameCanvas.onmousemove = (e) => {
 		console.info(e);
+
+		let pos = gridSnapPosition({x:e.offsetX, y:e.offsetY});
+		pos.x -= 43 - 32; // half of stone image width - outer margin offset
+		pos.y -= 43 - 32; // half of stone image width - outer margin offset
+
 		spriteRenderer.render(GAME_BOARD_IMG_SRC, {x:0, y:0});
-		spriteRenderer.render(STONE_IMG_SRC, {x:e.offsetX-43, y:e.offsetY-43});
+		spriteRenderer.drawGrid(9, 64, 32, 'black');
+		spriteRenderer.render(STONE_IMG_SRC, pos);
 	};
-};
+}
+
+function gridSnapPosition(pos) {
+	let col = Math.floor((pos.x) / 64);
+	let row = Math.floor((pos.y) / 64);
+
+	if (col < 0) {
+		col = 0;
+	} else if (col > 8) {
+		col = 8;
+	}
+
+	if (row < 0) {
+		row = 0;
+	} else if (row > 8) {
+		row = 8;
+	}
+
+	console.info(col + ',' + row);
+	return {x:col*64, y:row*64};
+}
 
 window.onload = setup;
 
