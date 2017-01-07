b67e4b6968ed35fe509c131132dfd15ea2d4cc5f
Sam Vaughn <cornerkitten@users.noreply.github.com>
2017-01-07 00:08:39 -0500

    Refactor private property style in SpriteRenderer

--- js/components/SpriteRenderer.js
+++ js/components/SpriteRenderer.js
@@ -1,37 +1,37 @@
 
 // Private properties
-let _sprite = new WeakMap();
-let _alpha = new WeakMap();
-let _origin = new WeakMap();
+let sprite_ = Symbol('sprite');
+let alpha_ = Symbol('alpha');
+let origin_ = Symbol('origin');
 
 export default class SpriteRenderer {
 	constructor(sprite, alpha) {
-		_sprite.set(this, sprite);
+		this[sprite_] = sprite;
 
 		if (alpha === undefined) {
 			alpha = 1;
 		}
-		_alpha.set(this, alpha);
-		_origin.set(this, {x: 0, y: 0});
+		this[alpha_] = alpha;
+		this[origin_] = {x: 0, y: 0};
 	}
 
 	get sprite() {
-		return _sprite.get(this);
+		return this[sprite_];
 	}
 
 	set sprite(newSprite) {
-		_sprite.set(this, newSprite);
+		this[sprite_] = newSprite;
 	}
 
 	get alpha() {
-		return _alpha.get(this);
+		return this[alpha_];
 	}
 
 	set alpha(newAlpha) {
-		return _alpha.set(this, newAlpha);
+		return this[alpha_] = newAlpha;
 	}
 
 	get origin() {
-		return _origin.get(this);
+		return this[origin_];
 	}
 }
