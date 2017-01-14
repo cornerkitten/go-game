7063fab94aea55ca356b218870567b20d9168527
Sam Vaughn <cornerkitten@users.noreply.github.com>
2017-01-14 00:47:25 -0500

    Add texture loading with Pixi.loader
     - Add texture loading logic to Engine
    
     - Add textures.js for referencing textures and respective file paths
    
     - Change width and height to be 128px, in both go stone SVG files

--- src/go/core/Engine.js
+++ src/go/core/Engine.js
@@ -1,10 +1,22 @@
 
+// TODO Remove eslint debugging settings
+/* eslint no-console: "off" */
+/* eslint no-debugger: "off" */
+
 /* global requestAnimationFrame */
 
 import * as Pixi from 'pixi.js';
+import textures from '../resources/textures';
 
 const stage_ = Symbol('stage');
 const renderer_ = Symbol('renderer');
+
+function loadTextures(callback) {
+  const textureFiles = Object.keys(textures).map(key => textures[key]);
+  textureFiles.forEach(file => Pixi.loader.add(file));
+  Pixi.loader.once('complete', callback);
+  Pixi.loader.load();
+}
 
 export default class Engine {
   constructor() {
@@ -12,16 +24,26 @@ export default class Engine {
     this[renderer_] = undefined;
   }
 
+  // TODO Remove default width and height
+  // TODO Ensure width and height is dynamically calculated based on viewport
   init(view, width = 800, height = 600) {
     this[renderer_] = Pixi.autoDetectRenderer(width, height, {
       view,
       transparent: true,
     });
 
+    loadTextures(this.onLoad.bind(this));
+  }
+
+  onLoad() {
+    const stone = Pixi.Sprite.from(textures.BLACK_STONE);
+    this[stage_].addChild(stone);
     requestAnimationFrame(this.update.bind(this));
   }
 
   update() {
     this[renderer_].render(this[stage_]);
+
+    requestAnimationFrame(this.update.bind(this));
   }
 }
---
+++ src/go/resources/textures.js
@@ -0,0 +1,5 @@
+
+export default {
+  BLACK_STONE: 'static/img/go-stone-black-flat.svg',
+  WHITE_STONE: 'static/img/go-stone-white-flat.svg',
+};
